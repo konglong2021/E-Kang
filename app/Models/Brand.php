a2da025c9518f80506ab8408dbae0fe4b3109614
unknown <lykonglong@yahoo.com>
2021-09-19 13:39:49 +0700

    fix model casensitive

---
+++ app/Models/Brand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Brand extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = [
+        'name',
+       
+    ];
+    
+    /**
+     * Get the products that owns the brand
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    use softDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'code',
+        'en_name',
+        'kh_name',
+        'description',
+        'image',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class,'category_id', 'id');
+    }
+
+    public function brands()
+    {
+        return $this->belongsToMany(Brand::class);
+    }
+
+ 
+}
